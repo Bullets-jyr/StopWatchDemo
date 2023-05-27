4220ba67bd717dc54794b05ac60501f83263c44d
Bullets-jyr <jyr@bullets.co.kr>
2023-05-27 22:09:06 +0900

    154. Android Services and Broadcast Receivers : Stopwatch app with view binding

--- app/src/main/java/kr/co/bullets/stopwatchdemo/MainActivity.kt
+++ app/src/main/java/kr/co/bullets/stopwatchdemo/MainActivity.kt
@@ -1,11 +1,79 @@
 package kr.co.bullets.stopwatchdemo
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.bullets.stopwatchdemo.databinding.ActivityMainBinding
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var isStarted = false
+    private lateinit var serviceIntent : Intent
+    private var time = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStart.setOnClickListener {
+            startOrStop()
+        }
+        binding.btnReset.setOnClickListener {
+            reset()
+        }
+
+        serviceIntent = Intent(applicationContext, StopWatchService::class.java)
+        // Inside the MainActivity, we will create a broadCast receiver to receive this breadCase
+        registerReceiver(updateTime, IntentFilter(StopWatchService.UPDATED_TIME))
+    }
+
+    private fun startOrStop() {
+        if (isStarted)
+            stop()
+        else
+            start()
+    }
+
+    private fun start() {
+        serviceIntent.putExtra(StopWatchService.CURRENT_TIME, time)
+        startService(serviceIntent)
+
+        binding.btnStart.text = "Stop"
+        isStarted = true
+    }
+
+    private fun stop() {
+        stopService(serviceIntent)
+
+        binding.btnStart.text = "Start"
+        isStarted = false
+    }
+
+    private fun reset() {
+        stop()
+
+        time = 0.0
+        binding.tvTime.text = getFormattedTime(time)
+    }
+
+    private val updateTime : BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            time = intent.getDoubleExtra(StopWatchService.CURRENT_TIME,0.0)
+            binding.tvTime.text = getFormattedTime(time)
+        }
+    }
+
+    private fun getFormattedTime(time: Double): String {
+        val timeInt = time.roundToInt()
+        val hours = timeInt % 86400 / 3600
+        val minutes = timeInt % 86400 % 3600 / 60
+        val seconds = timeInt % 86400 % 3600 % 60
+
+        return String.format("%02d:%02d:%02d", hours, minutes, seconds)
     }
 }
---
+++ app/src/main/java/kr/co/bullets/stopwatchdemo/StopWatchService.kt
@@ -0,0 +1,38 @@
+package kr.co.bullets.stopwatchdemo
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import java.util.Timer
+import java.util.TimerTask
+
+class StopWatchService : Service() {
+    private val timer = Timer()
+
+    override fun onBind(intent: Intent): IBinder? = null
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        val time = intent.getDoubleExtra(CURRENT_TIME,0.0)
+        timer.scheduleAtFixedRate(StopWatchTimerTask(time),0,1000)
+        return START_NOT_STICKY
+    }
+
+    override fun onDestroy() {
+        timer.cancel()
+        super.onDestroy()
+    }
+
+    companion object{
+        const val CURRENT_TIME = "current time"
+        const val UPDATED_TIME = "updated time"
+    }
+
+    private inner class StopWatchTimerTask(private var time: Double): TimerTask(){
+        override fun run() {
+            val intent = Intent(UPDATED_TIME)
+            time++
+            intent.putExtra(CURRENT_TIME, time)
+            sendBroadcast(intent)
+        }
+    }
+}
